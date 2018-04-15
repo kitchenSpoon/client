313314f07b1a29d23b59f224524a366409e945da
Matt Huggins <matt.huggins@gmail.com>
2018-04-15 08:41:38 -0500

    WIP more address bar fixes

--- src/components/AuthenticatedLayout/AddressBar/index.js
+++ src/components/AuthenticatedLayout/AddressBar/index.js
@@ -1,19 +1,27 @@
-import { compose, withProps } from 'recompose';
+import { compose, withState, withProps, withPropsOnChange } from 'recompose';
 import { withRouter } from 'react-router-dom';
-import { withData } from 'spunky';
+import { withData, withProgress, progressValues } from 'spunky';
 
 import AddressBar from './AddressBar';
 import nameServiceActions from '../../../actions/nameServiceActions';
 
+const { LOADED } = progressValues;
+
 const mapNameServiceDataToProps = (data) => ({
-  query: data && data.query,
+  nameServiceQuery: data && data.query,
   target: data && data.target
 });
 
 export default compose(
   withRouter,
+  withData(nameServiceActions, mapNameServiceDataToProps),
+  withProgress(nameServiceActions),
   withProps((props) => ({
     doQuery: (query) => props.history.push(`/browser/${encodeURIComponent(query)}`)
   })),
-  withData(nameServiceActions, mapNameServiceDataToProps)
+  withState('query', 'setQuery', ({ nameServiceQuery }) => nameServiceQuery || 'nos.neo'),
+  withPropsOnChange(
+    (props, nextProps) => props.progress !== LOADED && nextProps.progress === LOADED,
+    (props) => ({ query: props.nameServiceQuery })
+  )
 )(AddressBar);
